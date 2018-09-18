2abf58863085e5ebd7cb50b94e646fb36eb59995
HamzaJirah <hamzajirah@gmail.com>
2018-09-18 20:17:39 +0100

    modified file

--- ajax.js
+++ ajax.js
@@ -21,7 +21,7 @@ function renderText(){
 
   //USING OnReadyStateChange FUNCTION
   xhr.onreadystatechange = function(){
-    console.log("READYSTATE ", xhr.readyState);
+    console.log("READYSTATE ", this.readyState);
     if(this.readyState == 4 && this.status == 200){
       // console.log(this.responseText);
     }
