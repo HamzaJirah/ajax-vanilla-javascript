9125f74e1736305311a403e7e5173ee8b4167f6a
HamzaJirah <hamzajirah@gmail.com>
2018-09-18 19:36:35 +0100

    debugged the create xml object method

--- ajax.js
+++ ajax.js
@@ -4,8 +4,7 @@ document.querySelector('#button').addEventListener('click', renderText)
 //create loadText function
 function renderText(){
   //Create XHRobject
-  let xhr = new DOMParser();
-      xhr = XMLHttpRequest();
+   const xhr = new XMLHttpRequest();
   //OPEN-type, url/file, aync
   xhr.open('GET', 'text.txt', true);
 
