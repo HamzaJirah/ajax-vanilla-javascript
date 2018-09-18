f8ad2ac1c143b1495b9e8b24316e245f10e79ffc
HamzaJirah <hamzajirah@gmail.com>
2018-09-18 20:06:56 +0100

    created comments

--- ajax.js
+++ ajax.js
@@ -26,6 +26,11 @@ function renderText(){
 }
 
 //Ready state values
+//0: "Request not initialized"
+//1: "server connection established"
+//2: "request received"
+//3: "processing request"
+//4: "request finished and response is ready"
 
 //HTTP Status
 //200: "OK"
