bacdcfb14b8b29afb7c154f40e14abf8fb965b4b
HamzaJirah <hamzajirah@gmail.com>
2018-09-26 06:12:30 +0100

    changed variable name

--- ajax3.js
+++ ajax3.js
@@ -1,6 +1,6 @@
 //select elements from html document
 const button = document.querySelector('#button');
-const users = document.querySelector('#users');
+const displayUsers = document.querySelector('#users');
 
 //addEventListener to button variable
 button.addEventListener('click', renderUsers);
